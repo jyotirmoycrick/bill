9f9f58c31196c552614d944b39764fcbaeae2ddb
jyotirmoycrick <112798691+jyotirmoycrick@users.noreply.github.com>
2023-08-02 21:08:20 +0530

    Update car.c

--- car.c
+++ car.c
@@ -1,51 +1,138 @@
-#include<stdio.h>
-int main()
-{
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <title>Parking Bill Calculator</title>
 
-    int vehicle;
+    <style>
 
-    printf("Types of vehicle \n");
 
-    printf("1 for heavy vehicle \n");
-
-    printf("2 for personal car \n");
-
-    printf("3 for two wheeler \n");
-
-    printf("enter the vehicle type allot number : ");
-
-    scanf("%d",&vehicle);
-
-    int time;
-
-    printf("enter the parking time in hours : ");
-    scanf("%d",&time);
-
-    switch(vehicle)
-    {
-        case 1: printf("your vehicle type is heavy \n");
-
-        printf("Your total bill : %d \n",20*time );
-
-        break;
-
-        case 2: printf("your vehicle type is personal car \n");
-
-        printf("Your total bill : %d \n",10*time);
-
-        break;
-
-        case 3: printf("your vehicle type is two wheller \n");
-
-        printf("Your total bill : %d \n",5*time);
-
-        break;
-
-        default : printf("you don't need to pay anything your parking is free \n");
-    }
-
-    printf("Thank you \n");
-
-    return 0;
-
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f2f2f2;
 }
+
+.container {
+    max-width: 400px;
+    margin: auto;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 6px;
+    text-align: center;
+}
+
+h1 {
+    margin-top: 0;
+    color: #333;
+    text-align: center;
+}
+
+p {
+    color: #666;
+    text-align: center;
+}
+
+label {
+    display: block;
+    margin: 10px 0;
+    text-align: left;
+}
+
+select, input {
+    width: 100%;
+    padding: 8px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+button {
+    margin-top: 10px;
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: #4CAF50;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #45a049;
+}
+
+#result {
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 20px;
+}
+
+.ok
+{
+    margin-top: 30px;
+}
+
+.ok1
+{
+    margin-top: 20px;
+}
+
+
+    </style>
+
+</head>
+<body>
+    <div class="container">
+    <h1>Calculate your parking charge</h1>
+    <p>Parking charge calculator</p>
+
+    <label for="vehicle-type" class="ok">Select Vehicle Type: </label>
+    <select id="vehicle-type" >
+        <option value="1">Heavy Vehicle</option>
+        <option value="2">Personal Car</option>
+        <option value="3">Two-Wheeler</option>
+    </select>
+
+    <label for="parking-time" class="ok1">Enter the parking time in hours: </label>
+    <input type="number" id="parking-time" min="0">
+
+    <button onclick="calculateBill()">Calculate</button>
+
+    <p id="result"></p>
+
+    <script>
+        function calculateBill() {
+            const vehicle = parseInt(document.getElementById("vehicle-type").value);
+            const time = parseInt(document.getElementById("parking-time").value);
+            let bill = 0;
+
+            switch(vehicle) {
+                case 1:
+                    bill = 20 * time;
+                    break;
+                case 2:
+                    bill = 10 * time;
+                    break;
+                case 3:
+                    bill = 5 * time;
+                    break;
+                default:
+                    bill = 0;
+            }
+
+            const resultElement = document.getElementById("result");
+            if (bill > 0) {
+                resultElement.textContent = `Your total bill: $${bill}`;
+            } else {
+                resultElement.textContent = "You don't need to pay anything, your parking is free.";
+            }
+        }
+    </script>
+    </div>
+</body>
+</html>
